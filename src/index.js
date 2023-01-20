d171bbdb03903998d67cb9e683cc790dcc49d4b9
Aleksey100ckiy <109811250+Aleksey100ckiy@users.noreply.github.com>
2023-01-21 01:11:41 +0200

    create 11HW

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const KEY = '33003809-0ba39c85a11eed1272aa84bba';
+const BASE_URL ='https://pixabay.com/api/';
+
+
+
+
+/* <button type="button" class="load-more">Load more</button> */
+
+// const { height: cardHeight } = document
+//   .querySelector(".gallery")
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: "smooth",
+// });
+
+// "Sorry, there are no images matching your search query. Please try again."
+
+// "We're sorry, but you've reached the end of search results."
+
+// "Hooray! We found totalHits images."
